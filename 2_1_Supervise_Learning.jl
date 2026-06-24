ff1cda1db1dec68daf51736f3e9b0b26ce37b9c5
Miraj Samarakkody <miraj.samarakkody@gmail.com>
2026-06-24 15:24:50 -0500

    update ipynb

--- 2_1_Supervise_Learning.jl
+++ 2_1_Supervise_Learning.jl
@@ -1,10 +1,12 @@
 using Plots
+using Printf
 
 x = [0.03, 0.19, 0.34, 0.46, 0.78, 0.81, 1.08, 1.18, 1.39, 1.60, 1.65, 1.90]
 y = [0.67, 0.85, 1.05, 1.0, 1.40, 1.5, 1.3, 1.54, 1.55, 1.68, 1.73, 1.6]
 
 println(x)
 println(y)
+
 
 # Define 1D linear regression model 
 function f(x, ϕ₀, ϕ₁)
@@ -27,4 +29,22 @@ function plot_model(x, y, ϕ₀, ϕ₁)
     plot!(x_line, y_line, color = :red, lw = 2)
 end
 
+
 plot_model(x, y, 0.82, 0.52)
+
+# Function to calculate the loss (mean squared error) for given parameters
+function compute_loss(x, y, ϕ₀, ϕ₁)
+    loss = sum((f(x, ϕ₀, ϕ₁) .- y).^2)
+    return loss
+end
+
+loss = compute_loss(x, y, 0.4, 0.2)
+@printf("Your Loss = %3.2f, Ground truth =7.07\n", loss)
+
+# Set the intercept and slope as in figure 2.2c
+ϕ₀ = 1.60; ϕ₁ = -0.8
+# Plot the data and the model 
+plot_model(x, y, ϕ₀, ϕ₁)
+loss = compute_loss(x, y, ϕ₀, ϕ₁)
+@printf("Your Loss = %3.2f, Ground truth = 10.28\n", loss)
+
