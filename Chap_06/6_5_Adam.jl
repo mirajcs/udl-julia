7133e34e9767ad5867fa4173bd4c4d1d96015275
Miraj Samarakkody <miraj.samarakkody@gmail.com>
2026-09-07 07:59:59 -0500

    feat: created notebook 6.5

---
+++ Chap_06/6_5_Adam.jl
@@ -0,0 +1,31 @@
+### A Pluto.jl notebook ###
+# v1.0.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ cfefe42a-aabb-11f1-9e20-b3ba54431c78
+md"Notebook 6.5 - Adam
+
+This notebook investigates the Adam algorithm as illustrated in figure 6.9 from the book. "
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.7"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─cfefe42a-aabb-11f1-9e20-b3ba54431c78
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
