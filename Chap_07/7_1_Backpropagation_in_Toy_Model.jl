d5ab306bd09e7aca6bf952f15df8e58f0b8981ce
Miraj Samarakkody <miraj.samarakkody@gmail.com>
2026-09-10 09:27:50 -0500

    feat: notebook 7.1

---
+++ Chap_07/7_1_Backpropagation_in_Toy_Model.jl
@@ -0,0 +1,31 @@
+### A Pluto.jl notebook ###
+# v1.0.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6850b278-ad23-11f1-9aaa-f51c2826759c
+md"# Notebook 7.1 - Backpropagation in Toy Model
+
+This notebook computes the derivatives of the toy function discussed in section 7.3 of the book."
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.7"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─6850b278-ad23-11f1-9aaa-f51c2826759c
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
