648fc9f85b2132b7870ac6f4ac3b2b1a39364255
Miraj Samarakkody <miraj.samarakkody@gmail.com>
2026-09-13 16:42:47 -0500

    feat: created notebook 7.2

---
+++ Chap_07/7_2_Backpropagation.jl
@@ -0,0 +1,29 @@
+### A Pluto.jl notebook ###
+# v1.0.3
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ f842d82e-afbb-11f1-8b90-17d84ca8f2ba
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.12.7"
+manifest_format = "2.0"
+project_hash = "71853c6197a6a7f222db0f1978c7cb232b87c5ee"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═f842d82e-afbb-11f1-8b90-17d84ca8f2ba
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
